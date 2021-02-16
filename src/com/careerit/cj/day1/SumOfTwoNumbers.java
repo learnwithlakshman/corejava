c84b363bab12f9b46efb76a364f57957b3658f23
Lakshman <lakshman.miani@mail.com>
2021-02-16 08:25:23 +0530

    Added sub operation

--- src/com/careerit/cj/day1/SumOfTwoNumbers.java
+++ src/com/careerit/cj/day1/SumOfTwoNumbers.java
@@ -6,7 +6,10 @@ public class SumOfTwoNumbers {
 			int num1 = 10;
 			int num2 = 20;
 			int sum = num1 + num2;
+			int sub = num1 - num2;
 			System.out.print("The sum of "+num1+" + "+num2 +" = "+sum);
+			System.out.print("The sub of "+num1+" - "+num2 +" = "+sub);
+			
 			
 		}
 }
