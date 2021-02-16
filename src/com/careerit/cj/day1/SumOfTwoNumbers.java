e893344f4eab74ff382461376225aedc138bfffb
Lakshman <lakshman.miani@mail.com>
2021-02-16 08:21:42 +0530

    First git java project

---
+++ src/com/careerit/cj/day1/SumOfTwoNumbers.java
@@ -0,0 +1,12 @@
+package com.careerit.cj.day1;
+
+public class SumOfTwoNumbers {
+
+		public static void main(String[] args) {
+			int num1 = 10;
+			int num2 = 20;
+			int sum = num1 + num2;
+			System.out.print("The sum of "+num1+" + "+num2 +" = "+sum);
+			
+		}
+}
